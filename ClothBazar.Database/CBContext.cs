23fda7575022f4f9e6b9cdee433e8f7b6dfbc85e
Yeahia4422 <mdyeahia.khan@gmail.com>
2020-12-10 21:24:10 +0600

    Add product,category,baseentities,migrations

---
+++ ClothBazar.Database/CBContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClothBazar.Entities;
+
+namespace ClothBazar.Database
+{
+    class CBContext:DbContext
+    {
+        public CBContext() : base("ClothBazarConnection")
+        {
+
+        }
+
+        public DbSet<Product>Products { get; set; }
+        public DbSet<Category>Categories { get; set; }
+    }
+}
